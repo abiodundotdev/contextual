c09012707e8189154a2a5652afa764f093f52c62
Qazeem Abiodun <qudusqazeem@gmail.com>
2023-09-24 22:09:10 +0100

    Added theme and scaffold

--- lib/contextual.dart
+++ lib/contextual.dart
@@ -1,6 +1,6 @@
 library contextual;
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 extension XNavigation on BuildContext {
   NavigatorState get navigation => Navigator.of(this);
@@ -8,4 +8,21 @@ extension XNavigation on BuildContext {
 
 extension XMediaQuery on BuildContext {
   MediaQueryData get mediaQuery => MediaQuery.of(this);
+}
+
+extension XForm on BuildContext {
+  FormState get form => Form.of(this);
+}
+
+extension XNavigationRailThemeData on BuildContext {
+  NavigationRailThemeData get navigationRailThemeData =>
+      NavigationRailTheme.of(this);
+}
+
+extension XTheme on BuildContext {
+  ThemeData get theme => Theme.of(this);
+}
+
+extension XScaffold on BuildContext {
+  ScaffoldState get scaffold => Scaffold.of(this);
 }
